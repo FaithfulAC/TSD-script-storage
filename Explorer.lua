02a89049f8ca9c50761593c8978a0ec47fac3009
Shinkou Incorporated <135273047+FaithfulAC@users.noreply.github.com>
2024-11-10 20:26:30 -0800

    Update Explorer.lua

--- Explorer.lua
+++ Explorer.lua
@@ -663,7 +663,12 @@ end
 -- Total icons: 147 x 1 (147)
 
 local Icon do
-	local iconMap = "rbxasset://textures/ClassImages.png"
+	local iconMap;
+	if not getcustomasset then
+		iconMap = "rbxasset://textures/ClassImages.png"
+	else
+		iconMap = getcustomasset(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/refs/heads/main/DexAssets/ClassImages.png"))
+	end
 	local floor = math.floor
 
 	function Icon(IconFrame,index)
@@ -705,7 +710,12 @@ end
 -- Total icons: 1000
 
 local Icon2 do
-	local iconMap2 = "rbxasset://textures/TagEditor/famfamfam.png"
+	local iconMap2;
+	if not getcustomasset then
+		iconMap2 = "rbxasset://textures/TagEditor/famfamfam.png"
+	else
+		iconMap2 = getcustomasset(game:HttpGet("https://raw.githubusercontent.com/FaithfulAC/TSD-script-storage/refs/heads/main/DexAssets/famfamfam.png"))
+	end
 	local floor = math.floor
 
 	local iconDehash do
